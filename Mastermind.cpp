3c64d4d1ab25cd013fc277a2df5376d32d4eb8a0
Wachamuli <josemrr27@gmail.com>
2023-02-10 20:46:23 -0600

    logica base

--- Mastermind.cpp
+++ Mastermind.cpp
@@ -1,20 +1,58 @@
-// Mastermind.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+int main() 
 {
-    std::cout << "Hello World!\n";
+	int codigo[4], jugadas[4], i, j, vi, vj, adivina;
+	char pista[4];
+	std::cout << "Este programa intenta imitar el juego MasterMind \n";
+	std::cout << "Introduzca su combinacion a adivinar";
+
+	for (i = 0; i < 4; i++)
+	{
+		std::cout << "\n Introduzca el valor " << i + 1 << ":";
+		std::cin >> codigo[i];
+	}
+
+	std::cout << "\n Introduzca sus Jugadas para adivinar la clave";
+
+	for (i = 0; i < 10; i++) {
+		std::cout << "Intento " << i + 1 << "\n";
+		for (j = 0; j < 4; j++) {
+			std::cout << "Introduzca el valor para la columna " << j + 1 << ":";
+			std::cin >> jugadas[j];
+		}
+		// Verificacion de jugadas
+		adivina = 0;
+
+		for (vi = 0; vi < 4; vi++) 
+		{
+			pista[vi] = 'X';
+
+			for (vj = 0;vj < 4; vj++) 
+			{
+				// Verificacion de jugada frente a frente
+				if ((jugadas[vj] == codigo[vi]) && (vi == vj)) 
+					pista[vi] = 'C';
+				// verificacion de combinacion esta pero no frente a frente
+				else if ((jugadas[vj] == codigo[vi]) && (vi != vj)) 
+					pista[vi] = 'F';
+			}
+		}
+		for (vi = 0; vi < 4;vi++) if (pista[vi] == 'C') adivina++;
+
+		if (adivina == 4) 
+		{
+			std::cout << "ganaste en" << i << "intentos";
+			i = 10;
+		}
+		else 
+		{
+			for (vi = 0; vi < 4; vi++) 
+				std::cout << pista[vi] << " ";
+		}
+
+		std::cout << "Las coincidencia fueron: " << adivina;
+
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
